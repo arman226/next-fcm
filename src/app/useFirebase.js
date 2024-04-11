0f39e30416d55b10eccc2836403a7e8e485ba5b8
armando bibay jr <armando.bibay@bcstechnology.com.aucelar>
2024-04-11 14:18:57 +0800

    Create instance of firebase and listeners

--- src/app/useFirebase.js
+++ src/app/useFirebase.js
@@ -1,39 +1,3 @@
-# A PROOF OF CONCEPT OF CLIENT-SIDE IMPLEMENTATION OF FIREBASE CLOUD MESSAGING THROUGH NEXT JS
-
-## Install firebase sdk
-
-```bash
-npm install firebase
-```
-
-## Initialize a Firebase app instance
-
-### Create a <i>firebase.js</i> with the following content:
-
-```javascript
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  //these values are extracted from firebase console
-  //the actual values are stored in .env.local
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export default app;
-```
-
-### Create a <i>useFirebase.js</i> custom hook that handles token generation and notification listener:
-
-```javascript
 "use client";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 import { useEffect, useState } from "react";
@@ -89,10 +53,3 @@ const useFirebase = () => {
 };
 
 export default useFirebase;
-```
-
-### Fire the custom hook on the page file for it to <i>run</i> and initialize an instance
-
-```javascript
-const { fcmToken, msg } = useFirebase();
-```
